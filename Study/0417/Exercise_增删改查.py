71435d5f6603a6aecaa9d6cbfb2b97ed931cd381
acmemax <m18570945640@163.com>
2019-04-17 13:22:45 +0800

    zsgc

---
+++ Study/0417/Exercise_增删改查.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:acmemax
+list = ["飘逸的云", "花凉忆成殇", "花殇", "倦鸟栖梧", "忘誋旋律", "扼晚风", "紫蝶夢", "彡丨king霸荒", "亡梦爱人"]
+for friend in list:
+    print(friend+"，你好！")
+
+# 在数组后面加一个新的元素 append()在类比列表后面加上一个新的元素
+list2 = ["飘逸的云", "花凉忆成殇", "花殇", "倦鸟栖梧", "忘誋旋律", "扼晚风", "紫蝶夢", "彡丨king霸荒", "亡梦爱人"]
+list2.append("acmemax")
+print(list2)
+
+# 使用insert（）可以将元素加在任何位置
+list3 = ["飘逸的云", "花凉忆成殇", "花殇", "倦鸟栖梧", "忘誋旋律", "扼晚风", "紫蝶夢", "彡丨king霸荒", "亡梦爱人"]
+list3.insert(2, "acmemax")
+print(list3)
+
+# 如果知道元素所在的位置  可以使用del删除元素
+list4 = ["飘逸的云", "花凉忆成殇", "花殇", "倦鸟栖梧", "忘誋旋律", "扼晚风", "紫蝶夢", "彡丨king霸荒", "亡梦爱人"]
+del list4[1]
+print(list4)
+
+# pop()方法可以从列表末尾删除元素
+list4.pop()
+print(list4)
+
+
+#remove()方法可以根据值来删除列表里面的元素
+list4.remove("扼晚风")
+print(list4)
