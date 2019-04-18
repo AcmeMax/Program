f1bf278f77646319ca88d10ba83c618cf4fc9d3d
acmemax <m18570945640@163.com>
2019-04-18 14:54:29 +0800

    0417

--- Study/0417/Exercise_操作列表.py
+++ Study/0417/Exercise_操作列表.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:AcmeMax
+
+# 用for循环去打印列表里面的元素
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+    print(magician.title() + " that was a great trick!")
+
+
+# 创建数值列表
+# python 使用range()可以创建一系列数字
+for i in range(1, 9):
+    print(i)
+
+# 使用range()生成一个列表
+List = list(range(1, 9))
+print(List)
+
+# range()的第三个参数是指出每次增长多少个数
+list1 = list(range(1, 20, 3))
+print(list1)
+
+
+# 创建一个列表,将1到10的平方加上去
+sqls = []
+for value in range(1, 11):
+    sql = value**2
+    sqls.append(sql)
+print(sqls)
+
+
+# 练习
+for value in range(1, 21):
+    print(value)
+
+sqles = list(range(1, 1000000))
+for value in sqles:
+    print(value, "\n")
--- Study/0417/Exercise_组织列表.py
+++ Study/0417/Exercise_组织列表.py
@@ -1,3 +1,60 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:AcmeMax
+
+
+# 使用sort对列表进行永久性的排序
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+
+# 给sort()加上参数reverse = True 输出的数组和正序的相反
+cars.sort(reverse=True)
+print(cars)
+
+# 使用sorted()对列表进行临时性的排序, 同样他也有reverse = True
+cars1 = ['bmw', 'audi', 'toyota', 'subaru']
+print("这是原来的列表:")
+print(cars1)
+print("这是临时修改的列表:")
+print(sorted(cars1))
+print(sorted(cars1, reverse=True))
+print("这个是经历了sorted()之后的列表:")
+print(cars1)
+
+
+# reverse()方法可以是列表倒序打印
+cars2 = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars2)
+cars2.reverse()
+print(cars2)
+
+
+# len()可以确定列表的长度
+cars3 = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars3))
+
+# 练习
+citys = ["sanya", "changshang", "beijing", "shanghai", "nanjing"]
+print(citys)
+
+print(sorted(citys))
+
+print(citys)
+
+print(sorted(citys, reverse=True))
+
+print(citys)
+
+citys.reverse()
+print(citys)
+
+citys.reverse()
+print(citys)
+
+citys.sort()
+print(citys)
+
+citys.sort(reverse=True)
+print(citys)
