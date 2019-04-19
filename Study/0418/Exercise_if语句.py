960c8116b9f987edc3fed3d7944d7b14a3182194
acmemax <m18570945640@163.com>
2019-04-19 08:41:06 +0800

    0419

---
+++ Study/0418/Exercise_if语句.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:acmemax
+cars = ["audi", "bwm", "subru", "toyota"]
+for car in cars:
+    if car == 'bwm':
+        print(car.upper())
+    else:
+        print(car.title())
